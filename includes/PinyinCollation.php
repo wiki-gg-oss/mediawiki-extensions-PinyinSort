a519c59445c2b95d49ca5b817d6f5289bbf00305
alex4401 <alex@wiki.gg>
2025-10-27 16:02:03 +0100

    PinyinCollation: Don't compute strlen of the character on each iteration
    Unused variable.

--- includes/PinyinCollation.php
+++ includes/PinyinCollation.php
@@ -21,7 +21,6 @@ class PinyinCollation extends Collation {
 		$builder = '';
 		for ( $i = 0; $i < $len; $i++ ) {
 			$char = mb_substr( $string, $i, 1, 'UTF-8' );
-			$charLen = strlen( $char );
 			if ( ord( $char[0] ) < 128 ) {
 				$builder .= $char;
 			} else if ( array_key_exists( $char, $convTable ) ) {
